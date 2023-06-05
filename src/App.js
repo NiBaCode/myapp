c133e60505eec77b5029f8ce966feaa39b456468
NiBaCode <nicolabartoccioni@gmail.com>
2023-06-05 09:14:29 +0200

    new dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import FirstForm from "./componets/FirstForm_Validation";
 import LeadList from "./componets/LeadList";
 import LeadDetails from "./componets/leadDetails";
+import Navigation from "./componets/Navigation";
+
 import { useEffect, useState } from "react";
 import {
   findAll,
@@ -8,10 +10,16 @@ import {
   deleteLead,
   updateLead,
 } from "./api/LeadApi";
-import { Alert, Grid, IconButton, Snackbar, Typography } from "@mui/material";
+import {
+  Alert,
+  AppBar,
+  Grid,
+  IconButton,
+  Snackbar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-
-
 
 let initialLead = {
   name: "",
@@ -24,21 +32,24 @@ let initialLead = {
 function App() {
   const [leadList, setLeadList] = useState([]);
   const [showLeadDetails, setShowLeadDetails] = useState(false);
-  const [currentLead, setCurrentLead] = useState(initialLead);
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const [showErrorAlert, setShowErrorAlert] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [currentLead, setCurrentLead] = useState({ ...initialLead });
+  const [snackbarStatus, setSnackbarStatus] = useState({
+    open: false,
+    message: "",
+    severity: "success",
+    duration: 2000,
+  });
 
   useEffect(() => {
     loadInitialData();
-  }, []);
+  }, [])
 
   const loadInitialData = () => {
     findAll({}).then((result) => {
       console.log("Risultato ottenuto dalla FETCH", result);
       setLeadList(result);
-    });
-  };
+    })
+  }
 
   const getLeadDetail = (leadId) => {
     console.log("Chiamata funzione di caricamento dettagli lead", leadId);
@@ -46,157 +57,90 @@ function App() {
       setCurrentLead(result);
       console.log(result);
       setShowLeadDetails(true);
-    });
-  };
+    })
+  }
 
   const hideLeadDetails = () => {
     setShowLeadDetails(false);
   };
 
   const resetForm = () => {
-    setCurrentLead(initialLead);
+    setCurrentLead({ ...initialLead });
   };
 
   const deleteSelectedLead = (leadId) => {
     deleteLead({ leadId }).then((result) => {
       loadInitialData();
-      setSnackbarMessage("Cancellazione avvenuta con successo");
-      setShowSuccessAlert(true);
+      setSnackbarStatus({
+        open: true,
+        message: "Cancellazione avvenuta con successo",
+        severity: "success",
+        duration: 2000,
+      });
     });
   };
 
   const updateSelectedLead = (lead) => {
     updateLead(lead).then((result) => {
-      setSnackbarMessage("Salvataggio avvenuto con successo");
       result?.leadId > 0
-        ? setShowSuccessAlert(true)
-        : setShowSuccessAlert(false);
+        ? setSnackbarStatus({
+            open: true,
+            message: "Salvataggio avvenuto con successo",
+            severity: "success",
+            duration: 2000,
+          })
+        : setSnackbarStatus({
+            ...snackbarStatus,
+            open: false,
+          });
       console.log("Risultato della chiamata rest UPDATE-LEAD", result);
       loadInitialData();
       resetForm();
+    })
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCurrentLead({
+      ...currentLead,
+      [name]: value,
     });
   };
 
-  const validateSelectedLead = (lead) => {
-
-    if (lead.name === null || lead.name === "") {
-      console.log("Name value is not null");
-      setSnackbarMessage("Inserire il nome del lead");
-      setShowErrorAlert(true);
-    } else if (lead.ownerName === null || lead.ownerName === "") {
-      console.log("ownerName value is not null");
-      setSnackbarMessage("Inserire l'owner del lead");
-      setShowErrorAlert(true);
-    } else if (lead.type === null || lead.type === "") {
-      console.log("ownerName value is not null");
-      setSnackbarMessage("Inserire il tipo del lead");
-      setShowErrorAlert(true);
-    }
-    if (
-      lead.name !== null &&
-      lead.ownerName !== null &&
-      lead.type !== null &&
-      lead.name !== "" &&
-      lead.ownerName !== "" &&
-      lead.type !== ""
-    ) {
-      console.log("Dati valorizzati correttamente");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, ownerName, type, leadId, groupId } = currentLead;
+    const lead = {
+      name,
+      ownerName,
+      type,
+      leadId,
+      groupId,
+    };
+    if (lead.name !== "" && lead.ownerName !== "" && lead.type !== "") {
       updateSelectedLead(lead);
     } else {
-      console.log('Validazione fallita');
+      setSnackbarStatus({
+        ...snackbarStatus,
+        open: true,
+        severity: "error",
+        message: "All fields are required.",
+      });
     }
   };
 
   return (
     <>
-      <Grid container spacing={3}>
-        <Grid item md={12}>
-          <Typography variant="h4" align="center">
-            MY APP
-          </Typography>
-        </Grid>
-        <Grid item md={4}>
-          <Typography variant="h6" align="center">
-            Inserimento Nuovo Lead
-          </Typography>
-          <FirstForm
-            lead={currentLead}
-            reset={resetForm}
-            validateLead={validateSelectedLead}
-          />
-        </Grid>
-        <Grid item md={8}>
-          <Typography variant="h6" align="center">
-            Elenco Leads
-          </Typography>
-          <LeadList
-            leadList={leadList}
-            getLeadDetail={getLeadDetail}
-            handleShowLeadDetails={setShowLeadDetails}
-            deleteLead={deleteSelectedLead}
-          />
-        </Grid>
-
-        <Snackbar
-          open={showSuccessAlert}
-          autoHideDuration={3000}
-          message="Note archived"
-          onClose={() => {
-            setShowSuccessAlert(false);
-          }}
-        >
-          <Alert
-            severity="success"
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setShowSuccessAlert(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-          >
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
-        <Snackbar
-          open={showErrorAlert}
-          autoHideDuration={5000}
-          message="Note archived"
-          onClose={() => {
-            setShowErrorAlert(false);
-          }}
-        >
-          <Alert
-            severity="error"
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setShowErrorAlert(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-          >
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
-      </Grid>
-
-      <hr />
-      {showLeadDetails ? (
-        <LeadDetails lead={currentLead} hideLeadDetails={hideLeadDetails} />
-      ) : (
-        <></>
-      )}
+      <Navigation
+        leadList={leadList}
+        getLeadDetail={getLeadDetail}
+        loadInitialData={loadInitialData}
+        deleteLead={deleteSelectedLead}
+      />
+      
+     
+     
+     
     </>
   );
 }
---
+++ src/componets/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Typography, Grid } from "@mui/material";
+
+
+const Dashboard = (props) => {
+  return (
+    <Box>
+     <Typography component ="h1" variant="h1">
+                                   Questa è la Dashboard
+                                    </Typography> 
+    </Box>
+  );
+};
+
+export default Dashboard;
