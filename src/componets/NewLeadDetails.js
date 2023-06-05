ebfba2de84b66467db6be50b4b79a85489c61999
NiBaCode <nicolabartoccioni@gmail.com>
2023-06-05 09:18:51 +0200

    new Leaddetails

---
+++ src/componets/NewLeadDetails.js
@@ -0,0 +1,18 @@
+import { Typography, Box} from '@mui/material';
+import React from 'react';
+
+// TODO catturare il parametro leadId dalla request
+//TODO recuperare il lead tramite il leadId ricevuto come parametro della request
+
+const NewLeadDetails = () => {
+            return(
+                                              
+                                     <Typography component ="h1" variant="h1">
+                                   Questo è LeadDetails
+                                    </Typography> 
+                        
+            )
+            
+}
+
+export default NewLeadDetails;
